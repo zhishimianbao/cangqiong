c0ffda3192cb91cc4379c5c7a09b50226480c548
hjx <1677518554@qq.com>
2025-06-30 21:50:53 +0800

    增加员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -15,4 +15,8 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Select("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
+            "values " +
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Employee employee);
 }
