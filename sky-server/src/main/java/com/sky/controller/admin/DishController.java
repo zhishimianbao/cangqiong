4721df8d4a9397f704f039bab485118ac90f4172
hjx <1677518554@qq.com>
2025-10-20 20:00:53 +0800

    wx小程序-商品浏览

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
@@ -101,10 +102,28 @@ public class DishController {
      * @param categoryId
      * @return
      */
+//    @GetMapping("/list")
+//    @ApiOperation("根据分类id查询菜品")
+//    public Result<List<Dish>> list(Long categoryId){
+//        List<Dish> list = dishService.list(categoryId);
+//        return Result.success(list);
+//    }
+
+    /**
+     * 根据分类id查询菜品
+     *
+     * @param categoryId
+     * @return
+     */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
-    public Result<List<Dish>> list(Long categoryId){
-        List<Dish> list = dishService.list(categoryId);
+    public Result<List<DishVO>> list(Long categoryId) {
+        Dish dish = new Dish();
+        dish.setCategoryId(categoryId);
+        dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
+
+        List<DishVO> list = dishService.listWithFlavor(dish);
+
         return Result.success(list);
     }
 
